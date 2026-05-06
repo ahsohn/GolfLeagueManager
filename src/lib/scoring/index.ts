6a5301ac5f3e52c14aa305e0b9cd2581916755d8
ahsohn <ahsohn@gmail.com>
2026-05-05 22:55:04 -0400

    feat(scoring): add scoring barrel export

---
+++ src/lib/scoring/index.ts
@@ -0,0 +1,5 @@
+export * from './types';
+export { findEventResult } from './find-event-result';
+export { classifyLineupResult } from './classify-lineup-result';
+export { mergeProposedResults } from './merge-proposed-results';
+export type { LineupRow, MergeResult } from './merge-proposed-results';
